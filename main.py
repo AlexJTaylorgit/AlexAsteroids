fb010f16ef2d86221a3fa04af788b2291c7b624f
AlexJTaylorgit <ajt9783@rit.edu>
2026-02-24 15:37:30 -0500

    2: Created the window, the refresh rate (60) and the exit command

--- main.py
+++ main.py
@@ -6,15 +6,20 @@ def main():
     print("Starting Asteroids with pygame version: 2.6")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    iteration_count = 1
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) #This line creates our Screen
+    iteration_count = 1 #This is a dummy number for my infinite loop (line below)
     while iteration_count > 0:
         log_state()
+        screen.fill("black")
+        pygame.display.flip() #Refreshes the Window
+        dt = clock.tick(60) / 1000 #Mesaures the time between refreshes of the screen
+
         for event in pygame.event.get():
-            screen.fill("black")
-            pygame.display.flip()
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT: #so I can manually close the window without ctrl-C in the terminal
                 return
 
             pass
